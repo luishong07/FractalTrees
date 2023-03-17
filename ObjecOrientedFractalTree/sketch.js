e4231baf26f50e9d3e21d21b4784c763a884cdf8
Luis Hong <luishong50@gmail.com>
2023-03-17 17:22:42 -0500

    implemented objec oriented version and growth of the tree by clicking; refactor original version and put it in a loop

--- ObjecOrientedFractalTree/sketch.js
+++ ObjecOrientedFractalTree/sketch.js
@@ -1,31 +1,44 @@
-let angle = 0
-let slider 
+let tree = [];
+let counter =0
+let leaves = []
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    slider = createSlider(0, TWO_PI, PI/4, 0.01)
-}
+    // slider = createSlider(0, TWO_PI, PI/4, 0.01)
+    let a = createVector(innerWidth / 2, innerHeight);
+    let b = createVector(innerWidth / 2, innerHeight - 100);
+    let root = new Branch(a, b);
 
+    tree[0] = root;
+}
+function mousePressed() {
+    // console.log("here");
+    for (let i = tree.length-1; i >=0; i--) {
+        if(!tree[i].finished){
+
+            tree.push(tree[i].branchRight())
+            tree.push(tree[i].branchLeft())
+        }
+        tree[i].finished = true
+    }
+    counter++
+    if(counter == 5){
+        for(let i = 0; i <tree.length; i++){
+            if(!tree[i].finished){
+                let leaf = tree[i].end.copy()
+                leaves.push(leaf)
+            }
+        }
+    }
+}
 function draw() {
-    background("black")
-    translate(innerWidth/2, innerHeight)//make origin at the bottom
-    
-    stroke('white')
-    branch(300)
-
-}
-
-function branch(length){
-    line(0, 0, 0, - length)
-    angle = slider.value()
-    translate(0, -length)
-    if(length > 4){
-        push()
-        rotate(angle)
-        branch(length*0.67)
-        pop()
-        push()
-        rotate(-angle)
-        branch(length*0.67)
-        pop()
+    background("black");
+    for (let i = 0; i < tree.length; i++) {
+        tree[i].show();
+        // tree[i].jitter()
+    }
+    for (let i = 0; i < leaves.length; i++) {
+        fill("pink")
+        ellipse(leaves[i].x, leaves[i].y, 8, 8)
+        // tree[i].jitter()
     }
 }
