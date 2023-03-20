33f5ad1323872435837fc22e5f329f253a4903da
Luis Hong <luishong50@gmail.com>
2023-03-19 23:32:14 -0500

    implemented constant cycle for the original version of the tree

--- OriginalVersion/sketch.js
+++ OriginalVersion/sketch.js
@@ -1,5 +1,4 @@
 let angle = 0
-let slider 
 function setup() {
     createCanvas(innerWidth, innerHeight);
     slider = createSlider(0, TWO_PI, PI/4, 0.01)
@@ -10,15 +9,21 @@ function draw() {
     translate(innerWidth/2, innerHeight)//make origin at the bottom
     
     stroke('white')
+    angle += 0.000001
+    // console.log(angle)
     branch(300)
+    // frameRate(30)
+
 
 }
 
 function branch(length){
+    // console.log(angle)
     line(0, 0, 0, - length)
-    angle = slider.value()
+    // angle = slider.value()
+    angle += 0.00001
     translate(0, -length)
-    if(length > 4){
+    if(length > 10){
         push()
         rotate(angle)
         branch(length*0.67)
